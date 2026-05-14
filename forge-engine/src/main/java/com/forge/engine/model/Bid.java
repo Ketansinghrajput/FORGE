ce19f0714cf549ac105ee0b9c00b3e8aaaa7e480
Ketansinghrajput <theketanrajput@gmail.com>
2026-05-14 18:53:05 +0530

    Forge: 14/05/26

--- forge-engine/src/main/java/com/forge/engine/model/Bid.java
+++ forge-engine/src/main/java/com/forge/engine/model/Bid.java
@@ -4,13 +4,11 @@ import java.time.Instant;
 
 public record Bid(BidKey bidKey, String bidderId) {
 
-    // Backward Compatibility Constructor: Tera purana code yahi constructor call karega
-    // aur yeh automatically naya BidKey object generate kar lega!
+
     public Bid(String bidderId, Money price) {
         this(new BidKey(price, Instant.now()), bidderId);
     }
 
-    // --- OLD GETTERS (Taaki tera bacha hua code break na ho) ---
     public Money getPrice() {
         return bidKey.amount();
     }
